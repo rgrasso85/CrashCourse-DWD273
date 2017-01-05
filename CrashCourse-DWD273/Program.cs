dafdc6c045e589d0b902ce992178167dc850ed27
rgrasso85 <rgrass2806@my.sctd.edu>
2017-01-05 09:24:43 -0500

    Variable Types added and printed to screen.

--- CrashCourse-DWD273/Program.cs
+++ CrashCourse-DWD273/Program.cs
@@ -11,6 +11,16 @@ namespace CrashCourse_DWD273
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            int intTest = 1;
+            double doubleTest = 1.1;
+            bool booleanTest = true;
+            char charTest = 'A';
+            string stringTest = "This is a String.";
+            Console.WriteLine("intTest = " + intTest);
+            Console.WriteLine("doubleTest = " + doubleTest);
+            Console.WriteLine("booleanTest = " + booleanTest);
+            Console.WriteLine("charTest = " + charTest);
+            Console.WriteLine("stringTest = " + stringTest);
             Console.Read();
         }
     }
